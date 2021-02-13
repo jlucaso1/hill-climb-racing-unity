4a309b99fbd2dff77ccd09dd6124090477e99e15
jlucaso <jlucaso@hotmail.com>
2021-02-13 13:37:26 -0300

    New main Menu

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NextLevel : MonoBehaviour
+{
+    public string sceneName;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Vehicle")
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject vehicle;
+    void Start()
+    {
+        GameObject vehicleInstannced= Instantiate(vehicle, new Vector3(5, 3, 0), Quaternion.identity);
+        vehicleInstannced.transform.SetParent(transform);
+    }
+}

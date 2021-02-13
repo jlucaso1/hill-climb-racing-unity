d511ede9ff0f446c265f542ef4199402c9e305bc
jlucaso <jlucaso@hotmail.com>
2021-02-13 20:01:24 -0300

    Refactored

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     public GameObject vehicle;
     void Awake()
     {
-        GameObject vehicleInstannced= Instantiate(Resources.Load<GameObject>("Prefabs/Vehicles/DefaultVehicle"), new Vector3(5, 3, 0), Quaternion.identity);
+        GameObject vehicleInstannced = Instantiate(Resources.Load<GameObject>("Prefabs/Vehicles/MotorCycle"), new Vector3(5, 3, 0), Quaternion.identity);
         vehicleInstannced.transform.SetParent(transform);
         GameObject.Find("PlayerCam").GetComponent<CinemachineVirtualCamera>().m_Follow = vehicleInstannced.transform;
     }
